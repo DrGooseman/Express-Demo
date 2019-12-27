20efea0aed7bd0bf674d543977e1d7b91a33a844
DrGooseman <jamesjquinn12@gmail.com>
2019-12-27 16:30:12 +0300

    Refactored into different files

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 const morgan = require('morgan');
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -13,6 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
+app.use('/api/courses', courses);
+app.use('/', home);
 
 console.log('Application Name: ' + config.get('name'));
 console.log('Mail Server: ' + config.get('mail.host'));
@@ -33,71 +37,6 @@ app.use(function(req, res, next) {
 	next();
 })
 
-const courses = [
-	{ id:1, name: 'course1'},
-	{ id:2, name: 'course2'},
-	{ id:3, name: 'course3'}
-]
-
-app.get('/', (req,res) => {
-	res.render('index', {title: 'My Express App', message: 'Hello'});
-});
-
-app.get('/api/courses', (req,res) => {
-	res.send(courses);
-});
-
-app.get('/api/courses/:id', (req,res) => {
-	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if (!course) return res.status(404).send("The course was not found");
-	res.send(course);
-});
-
-
-app.put('/api/courses/:id', (req,res) => {
-	const course = courses.find(c => c.id === parseInt(req.params.id));
-
-	if (!course) return res.status(404).send("The course was not found");
-
-	const { error } = validateCourse(req.body);
-
-	if (error) return res.status(400).send(error);
-
-	course.name = req.body.name;
-	res.send(course);
-});
-
-app.delete('/api/courses/:id', (req,res) => {
-	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if (!course) return res.status(404).send("The course was not found");
-
-	const index = courses.indexOf(course);
-	courses.splice(index, 1);
-	res.send(course);
-});
-
-
-app.post('/api/courses/', (req,res) => {
-	const { error } = validateCourse(req.body);
-
-	if (error) return res.status(400).send(error);
-
-	const course = {
-		id: courses.length + 1,
-		name: req.body.name
-	}
-	courses.push(course);
-	res.send(course);
-});
-
-function validateCourse(course)
-{
-	const schema = {
-		name: Joi.string().min(3).required()
-	};
-
-	return Joi.validate(course, schema);
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
